dccdaa28d574c1b5153ab5007ab9054836ed0dfb
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-10-25 00:29:14 +0200

    Commit

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -7,8 +7,9 @@ const preferDefault = m => (m && m.default) || m
 exports.components = {
   "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/Mohamed/Documents/Gatsby-Website/.cache/dev-404-page.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("/Users/Mohamed/Documents/Gatsby-Website/src/pages/404.js"))),
+  "component---src-pages-destinations-js": hot(preferDefault(require("/Users/Mohamed/Documents/Gatsby-Website/src/pages/destinations.js"))),
+  "component---src-pages-images-js": hot(preferDefault(require("/Users/Mohamed/Documents/Gatsby-Website/src/pages/Images.js"))),
   "component---src-pages-index-js": hot(preferDefault(require("/Users/Mohamed/Documents/Gatsby-Website/src/pages/index.js"))),
-  "component---src-pages-page-2-js": hot(preferDefault(require("/Users/Mohamed/Documents/Gatsby-Website/src/pages/page-2.js"))),
   "component---src-pages-using-typescript-tsx": hot(preferDefault(require("/Users/Mohamed/Documents/Gatsby-Website/src/pages/using-typescript.tsx")))
 }
 
