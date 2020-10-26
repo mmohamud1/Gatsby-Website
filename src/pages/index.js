49d1a97cc0c1e3a8b85a7083d6cb3dcd9def44f4
mmohamud1 <63135402+mmohamud1@users.noreply.github.com>
2020-10-26 19:59:32 +0200

    Commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,12 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import HeroSection from "../components/HeroSection"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Home page</h1>
+    <HeroSection />
   </Layout>
 )
 
